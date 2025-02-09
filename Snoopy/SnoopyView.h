3f7821872498eb7a7db18073d05dd6121d604643
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2025-02-09 15:32:29 -0500

    Initial Commit

---
+++ Snoopy/SnoopyView.h
@@ -0,0 +1,12 @@
+//
+//  SnoopyView.h
+//  Snoopy
+//
+//  Created by Yaxin Cheng on 2025-02-09.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SnoopyView : ScreenSaverView
+
+@end
---
+++ Snoopy/SnoopyView.m
@@ -0,0 +1,51 @@
+//
+//  SnoopyView.m
+//  Snoopy
+//
+//  Created by Yaxin Cheng on 2025-02-09.
+//
+
+#import "SnoopyView.h"
+
+@implementation SnoopyView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
