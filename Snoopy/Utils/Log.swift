ce2ef6babf065f21351f9e598280434e0ff380d9
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2025-05-31 14:20:59 -0700

    Updated log format to include file information

--- Snoopy/Utils/Log.swift
+++ Snoopy/Utils/Log.swift
@@ -14,22 +14,22 @@ enum Log {
     /// Logs a message at the debug level.
     public static func debug(_ message: String, file: String = #fileID, function: String = #function, line: Int = #line) {
         // Construct the final log message *inside* the wrapper
-        logger.debug("\(message) -- [\(function):\(line)]")
+        logger.debug("\(message) -- [\(file):\(line) - \(function)]")
     }
     
     /// Logs a message at the info level.
     public static func info(_ message: String, file: String = #fileID, function: String = #function, line: Int = #line) {
-        logger.info("\(message) -- [\(function):\(line)]")
+        logger.info("\(message) -- [\(file):\(line) - \(function)]")
     }
     
     /// Logs a message at the notice level (persisted by default).
     public static func notice(_ message: String, file: String = #fileID, function: String = #function, line: Int = #line) {
-        logger.notice("\(message) -- [\(function):\(line)]")
+        logger.notice("\(message) -- [\(file):\(line) - \(function)]")
     }
     
     /// Logs an error message, optionally including an Error object. (Persisted by default).
     public static func error(_ message: String, error: Error? = nil, file: String = #fileID, function: String = #function, line: Int = #line) {
-        let location = "[\(function):\(line)]" // Capture call site info
+        let location = "[\(file):\(line) - \(function)]" // Capture call site info
         
         if let error = error {
             // Log both the custom message and the error details
@@ -43,13 +43,13 @@ enum Log {
     
     /// Logs a critical fault message. (Persisted by default).
     public static func fault(_ message: String, file: String = #fileID, function: String = #function, line: Int = #line) -> Never {
-        logger.fault("\(message) -- [\(function):\(line)]")
-        fatalError("\(message) -- [\(function):\(line)]")
+        logger.fault("\(message) -- [\(file):\(line) - \(function)]")
+        fatalError("\(message) -- [\(file):\(line) - \(function)")
     }
 }
 
 /// unreachable indicates this part of the code should never be reached.
 /// This function will print out the message and exit the program.
 func unreachable(_ message: String, file: String = #fileID, function: String = #function, line: Int = #line) -> Never {
-    fatalError("[UNREACHABLE]: \(message) -- [\(function):\(line)]")
+    fatalError("[UNREACHABLE]: \(message) -- [\(file):\(line) - \(function)]")
 }
